2365e05f80708d6dc401cfccd6b0aa79dc762c49
shinyme1997-sudo <shinyme1997@gmail.com>
2026-02-03 12:56:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -68,7 +68,7 @@ const CONFIG = {
     // Adjust these if you want faster/slower animations
     animations: {
         floatDuration: "10s",           // How long it takes hearts to float up (10-20s recommended)
-        floatDistance: "70px",          // How far hearts move sideways (30-70px recommended)
+        floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.3s",            // Speed of bouncing animations (0.3-0.7s recommended)
         heartExplosionSize: 1.2         // Size of heart explosion effect (1.2-2.0 recommended)
     },
