b918ede1ba952e5d861911fc4ccad338f88bdae7
shinyme1997-sudo <shinyme1997@gmail.com>
2026-02-03 15:54:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -82,7 +82,13 @@ const CONFIG = {
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
-    }
+    },
+    
+    // Font customization
+    fontFamily: "'Poppins', cursive",
+    fontColor: "#FDECEF",
+
+   }
 };
 
 // Don't modify anything below this line unless you know what you're doing
